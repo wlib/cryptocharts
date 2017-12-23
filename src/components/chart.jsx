67bb3623f71c03efbfdb057d1c94c4c1343a5b6c
Daniel Ethridge <danielethridge@icloud.com>
2017-12-22 20:24:47 -0500

    /top now loads more after scrolling to bottom

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -2,7 +2,7 @@ import { Component, h } from "preact";
 import settings from "../settings";
 
 export default class Chart extends Component {
-  render({ symbol = "BTCUSD" }) {
+  render({ symbol = "BTC" }) {
     const theme = settings.night ? "Dark" : "Light";
     const widget = `https://www.tradingview.com/widgetembed/?symbol=${symbol}USD&interval=120&hidetoptoolbar=1&hideideas=1&theme=${theme}&style=8`;
     const fullChart = `https://www.tradingview.com/widgetembed/?symbol=${symbol}USD&interval=120&hidesidetoolbar=0&symboledit=1&saveimage=1&withdateranges=1&studies=MASimple%40tv-basicstudies&theme=${theme}&style=8`;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,22 +8,41 @@ import TokenPreviewList from "../components/token-preview-list";
 //import SearchPage from "./search";
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.scrollListener = function() {
+      if (innerHeight + scrollY >= document.body.offsetHeight) {
+        this.setState((prevState, props) => ({
+          loadLimit: prevState.loadLimit + 10
+        }));
+      }
+    }.bind(this);
+  }
+
   async componentDidMount() {
     const globalData = await getGlobalData(settings);
     const tickerData = await getTickerData("", settings);
     this.setState({
       globalData,
-      tickerData
+      tickerData,
+      loadLimit: settings.limit
     });
+
+    addEventListener("scroll", this.scrollListener);
   }
 
-  render(props, { globalData = {}, tickerData = [] }) {
+  componentWillUnmount() {
+    removeEventListener("scroll", this.scrollListener);
+  }
+
+  render(props, { globalData = {}, tickerData = [], loadLimit }) {
     const toggleNight = () => {
       settings.night = !settings.night;
       document.body.classList.toggle("night");
-    }
+    };
 
-    const topTokens = tickerData.slice(0, settings.limit);
+    const topTokens = tickerData.slice(0, loadLimit);
     const pinnedTokens = tickerData.filter(token =>
       settings.pinned.includes(token.id)
     );
@@ -41,7 +60,7 @@ export default class Index extends Component {
         <Router>
           <TokenPreviewList default path="/top" tickerData={topTokens} />
           <TokenPreviewList path="/pinned" tickerData={pinnedTokens} />
-{/*          <SearchPage path="/search" /> */}
+          {/*          <SearchPage path="/search" /> */}
         </Router>
       </div>
     );
